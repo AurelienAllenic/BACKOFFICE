4e7ff77a09c35d43d7aa2d09f4f9c38ca6a4d29c
Aurelien Allenic <aurelien.allenic@gmail.com>
2022-11-18 17:06:49 +0100

    (feat) added style for sections of homepage

--- front/src/components/HomePage.jsx
+++ front/src/components/HomePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Section1, Section2, Section3, Section4, Section5 } from '../utils/styles/Homepage'
 
 function HomePage() {
     return (
         <>
-            <section id='accueil'>Accueil</section>
-            <section id='bio'>Bio</section>
-            <section id='experience'>experience</section>
-            <section id='portfolio'>portfolio</section>
-            <section id='contact'>contact</section>
+            <Section1 id='accueil'>Accueil</Section1>
+            <Section2 id='bio'>Bio</Section2>
+            <Section3 id='experience'>experience</Section3>
+            <Section4 id='portfolio'>portfolio</Section4>
+            <Section5 id='contact'>contact</Section5>
         </>
     )
 }
---
+++ front/src/utils/styles/Homepage.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Section1 = styled.section`
+height: 100vh;
+width: 100%;
+background-color: #bdc3c7;
+opacity: 0.7;
+`
+export const Section2 = styled.section`
+height: 100vh;
+width: 100%;
+background-color: #3498db;
+opacity: 0.7;
+`
+export const Section3 = styled.section`
+height: 100vh;
+width: 100%;
+background-color: #2ecc71;
+opacity: 0.7;
+`
+export const Section4 = styled.section`
+height: 100vh;
+width: 100%;
+background-color: #16a085;
+opacity: 0.7;
+`
+export const Section5 = styled.section`
+height: 100vh;
+width: 100%;
+background-color: #e67e22;
+opacity: 0.7;
+`
